665fb293ca27c82aaa2b0bdebbb9a2b886bf422c
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-05-17 21:14:48 +0200

    Uncomment autoscaler

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -11,25 +11,25 @@ Sidekiq.configure_client do |config|
   config.redis = { size: 2 } # for web dyno
 end
 
-# if ENV['HEROKU_APP']
-#   require 'autoscaler/sidekiq'
-#   require 'autoscaler/heroku_scaler'
-#   heroku = Autoscaler::HerokuScaler.new('worker')
-#   heroku_reader = Autoscaler::HerokuScaler.new('worker_reader')
+if ENV['HEROKU_APP']
+  require 'autoscaler/sidekiq'
+  require 'autoscaler/heroku_scaler'
+  heroku = Autoscaler::HerokuScaler.new('worker')
+  heroku_reader = Autoscaler::HerokuScaler.new('worker_reader')
 
-#   Sidekiq.configure_client do |config|
-#     config.client_middleware do |chain|
-#       chain.add Autoscaler::Sidekiq::Client, 'logs' => heroku, 'logs-reader' => heroku_reader
-#     end
-#   end
+  Sidekiq.configure_client do |config|
+    config.client_middleware do |chain|
+      chain.add Autoscaler::Sidekiq::Client, 'logs' => heroku, 'logs-reader' => heroku_reader
+    end
+  end
 
-#   Sidekiq.configure_server do |config|
-#     config.client_middleware do |chain|
-#       chain.add Autoscaler::Sidekiq::Client, 'logs' => heroku, 'logs-reader' => heroku_reader
-#     end
-#     config.server_middleware do |chain|
-#       chain.add Autoscaler::Sidekiq::Server, heroku, 10, %w[logs]
-#       chain.add Autoscaler::Sidekiq::Server, heroku_reader, 10, %w[logs-reader]
-#     end
-#   end
-# end
+  Sidekiq.configure_server do |config|
+    config.client_middleware do |chain|
+      chain.add Autoscaler::Sidekiq::Client, 'logs' => heroku, 'logs-reader' => heroku_reader
+    end
+    config.server_middleware do |chain|
+      chain.add Autoscaler::Sidekiq::Server, heroku, 10, %w[logs]
+      chain.add Autoscaler::Sidekiq::Server, heroku_reader, 10, %w[logs-reader]
+    end
+  end
+end
