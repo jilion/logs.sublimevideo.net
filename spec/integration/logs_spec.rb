bac2731f8ed36432d8386857d9443f886e9bee1e
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-05-17 09:26:18 +0200

    Add integration spec

---
+++ spec/integration/logs_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe "Logs creation", :slow do
+  before { EdgecastWrapper.stub(:remove_log_file) { true } }
+
+  it "fetches logs, store, read and parse them" do
+    LogsCreatorWorker.perform_async
+    LogsCreatorWorker.drain
+    Log.should have(2).logs
+    LogReaderWorker.should have(2).jobs
+    LogReaderWorker.drain
+    LogLineParserWorker.should have(35707).jobs
+    LogLineParserWorker.drain
+    StatsHandlerWorker.should have(0).jobs
+  end
+end
