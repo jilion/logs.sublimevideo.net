43ef9f239250dbf09b00c194305ba6c118a6bcce
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-05-23 11:54:30 +0200

    More robust rescuing

--- app/wrappers/edgecast_wrapper.rb
+++ app/wrappers/edgecast_wrapper.rb
@@ -7,16 +7,18 @@ class EdgecastWrapper
   LOGS_PATH = ENV['EDGECAST_LOGS_PATH']
 
   def self.logs_filename
-    rescue_and_retry(4) { _sftp.dir.glob(LOGS_PATH, '*.gz').map(&:name).sort }
+    rescue_and_retry(5) { _sftp.dir.glob(LOGS_PATH, '*.gz').map(&:name).sort }
   end
 
   def self.log_file(filename)
-    rescue_and_retry(4) { @sftp_file = _sftp.file.open(_log_path(filename), "r") }
-    LogFile.open!(filename, @sftp_file.read) { |log_file| yield(log_file) }
+    rescue_and_retry(5) do
+      sftp_file = _sftp.file.open(_log_path(filename), "r")
+      LogFile.open!(filename, sftp_file.read) { |log_file| yield(log_file) }
+    end
   end
 
   def self.remove_log_file(filename)
-    rescue_and_retry(4) { _sftp.remove!(_log_path(filename)) }
+    rescue_and_retry(5) { _sftp.remove!(_log_path(filename)) }
   end
 
   private
