56354f1de7a721d78750e31de4e3e4fdc8cbe21e
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-10-21 08:44:12 +0200

    Fix https://www.honeybadger.io/inspector/p/1916/1896758

--- app/wrappers/edgecast_wrapper.rb
+++ app/wrappers/edgecast_wrapper.rb
@@ -3,11 +3,13 @@ class EdgecastWrapper
   LOGS_PATH = ENV['EDGECAST_LOGS_PATH']
 
   def self.logs_filename
-    _retry { _sftp.dir.glob(LOGS_PATH, '*.gz').map(&:name).sort }
+    rescue_and_retry(7) { _sftp.dir.glob(LOGS_PATH, '*.gz').map(&:name).sort }
+  rescue
+    []
   end
 
   def self.log_file(filename)
-    _retry do
+    rescue_and_retry(7) do
       sftp_file = _sftp.file.open(_log_path(filename), "r")
       LogFile.open!(filename, sftp_file.read) { |log_file| yield(log_file) }
     end
@@ -16,15 +18,10 @@ class EdgecastWrapper
   end
 
   def self.remove_log_file(filename)
-    _retry { _sftp.remove!(_log_path(filename)) }
+    rescue_and_retry(7) { _sftp.remove!(_log_path(filename)) }
   end
 
   private
-
-  def self._retry
-    rescue_and_retry(7) { yield }
-  rescue
-  end
 
   def self._log_path(filename)
     "#{LOGS_PATH}/#{filename}"
